4cd9a493db5f055f69d790aafb435f31c67eda44
BIT <BIT@DESKTOP-1KCBVQ0>
2019-10-18 19:43:37 +0900

    xml설정 & java Config설정

---
+++ hellospring2/src/main/java/kr/co/itcen/hellospring/config/WebConfig.java
@@ -0,0 +1,38 @@
+package kr.co.itcen.hellospring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/*
+ * @EnableWebMvc:
+ * 이 어노테이션 한줄의 기능은.
+ * xml설정법과 비교해서보자면, <mvc: ~~~ >가 붙는 혹은 해당 태그 내에 등록하는 
+ * validator, interceptors, conversionService, messageConverter등의 모든 설정들을 자동으로 만들어주는 역할을 한다.
+ * 
+ */
+
+@Configuration
+//@EnableWebMvc
+@ComponentScan({"kr.co.itcen.hellospring.controller"})
+public class WebConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		//viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setExposeContextBeansAsAttributes(true);
+	
+		return viewResolver;
+	}
+	
+	
+	
+	
+	
+}
---
+++ hellospring2/src/main/java/kr/co/itcen/hellospring/controller/BoardController.java
@@ -0,0 +1,40 @@
+package kr.co.itcen.hellospring.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * @RequestMapping Type + Method
+ *
+ */
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list() {
+		return "BoardController:list";
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/write", method=RequestMethod.POST)
+	//PostMapping("/write") 이건 무슨의미지?..
+	public String write(Map<String, Object> map) { //매개변수로 설정해둔 곳으로 값이 다들어감. map으로 해줬기에 map안에 전부 다들어감
+		return "redirect:/board/list";
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/view/{no}", method=RequestMethod.GET)
+	public String view(@PathVariable("no") Long no) { //매개변수로 설정해둔 곳으로 값이 다들어감. map으로 해줬기에 map안에 전부 다들어감
+		return "BoardController:view:" + no;
+	}
+	
+}
